a20316dbaf42c1ffdfc9fcd60d0c207e4f2848a0
kolade <chrisjay967@gmail.com>
2024-04-20 20:38:50 +0100

    testing

---
+++ src/app/services/page.jsx
@@ -0,0 +1,9 @@
+const Services = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center mt-10">Services Page</h1>
+    </div>
+  );
+};
+
+export default Services;
---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center mt-10">Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
